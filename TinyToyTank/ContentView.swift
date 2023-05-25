c7c002876a488cc512b512a46262a059502f8e69
Light2288 <davide.aliti@gmail.com>
2023-05-25 22:44:13 +0200

    first commit

--- TinyToyTank/ContentView.swift
+++ TinyToyTank/ContentView.swift
@@ -8,23 +8,123 @@
 import SwiftUI
 import RealityKit
 
+class ViewModel: ObservableObject {
+    var onTankLeftNotification: () -> Void = { }
+    var onTankRightNotification: () -> Void = { }
+    var onTankForwardNotification: () -> Void = { }
+    var onTurretLeftNotification: () -> Void = { }
+    var onTurretRightNotification: () -> Void = { }
+    var onCannonFireNotification: () -> Void = { }
+    var isActionPlaying: Bool = false
+}
+
 struct ContentView : View {
+    @StateObject var vm = ViewModel()
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(vm: vm).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        vm.onTurretLeftNotification()
+                    } label: {
+                        Image("TurretLeft")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                    Button {
+                        vm.onCannonFireNotification()
+                    } label: {
+                        Image("CannonFire")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                    Button {
+                        vm.onTurretRightNotification()
+                    } label: {
+                        Image("TurretRight")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                }
+                HStack {
+                    Button {
+                        vm.onTankLeftNotification()
+                    } label: {
+                        Image("TankLeft")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                    Button {
+                        vm.onTankForwardNotification()
+                    } label: {
+                        Image("TankForward")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                    Button {
+                        vm.onTankRightNotification()
+                    } label: {
+                        Image("TankRight")
+                            .resizable()
+                            .frame(width: 80, height: 80, alignment: .center)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
+    let vm: ViewModel
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let tankAnchor = try! TinyToyTank.load_TinyToyTank()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        tankAnchor.cannon?.setParent(tankAnchor.tank, preservingWorldTransform: true)
+        
+        tankAnchor.actions.actionComplete.onAction = { _ in
+            vm.isActionPlaying = false
+        }
+        
+        vm.onTankRightNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.tankRight.post()
+        }
+        vm.onTankLeftNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.tankLeft.post()
+        }
+        vm.onTankForwardNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.tankForward.post()
+        }
+        vm.onTurretRightNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.turretRight.post()
+        }
+        vm.onTurretLeftNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.turretLeft.post()
+        }
+        vm.onCannonFireNotification = {
+            guard !vm.isActionPlaying else { return }
+            vm.isActionPlaying = true
+            tankAnchor.notifications.cannonFire.post()
+        }
+        
+        arView.scene.anchors.append(tankAnchor)
         
         return arView
         
